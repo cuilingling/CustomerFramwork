49fecd09a62c15e599d3766f43badb575ec101ba
zhaoyang <942584964@qq.com>
2018-02-08 16:35:08 +0800

    第二次提交

---
+++ app/src/main/java/com/facebook/myapplication/activity/vscTestActivity.java
@@ -0,0 +1,15 @@
+package com.facebook.myapplication.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.facebook.myapplication.R;
+
+public class vscTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vsc_test);
+    }
+}
